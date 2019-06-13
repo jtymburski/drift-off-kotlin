545436184e7fcd2f3864cf3fa0a18df957e8a781
Jordan Tymburski <hello@jordantymburski.com>
2019-06-13 00:05:07 -0700

    Home activity fixes to pass tests

--- app/src/main/java/com/jordantymburski/driftoff/kotlin/presentation/HomeActivity.kt
+++ app/src/main/java/com/jordantymburski/driftoff/kotlin/presentation/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.jordantymburski.driftoff.kotlin.presentation
 
 import android.app.TimePickerDialog
 import android.os.Bundle
+import android.os.Handler
 import android.text.format.DateFormat
 import android.view.WindowManager
 import androidx.fragment.app.FragmentActivity
@@ -10,18 +11,25 @@ import com.jordantymburski.driftoff.kotlin.domain.model.AlarmInfo
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_home.*
 import java.util.Calendar
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class HomeActivity : FragmentActivity() {
     companion object {
         private const val DAY_START_HOUR = 8L // 8:00am inclusive
         private const val DAY_END_HOUR = 18L // 6:00pm inclusive
+        private val UPDATE_TIME_MS = TimeUnit.SECONDS.toMillis(30)
     }
 
     /**
      * Current theme resource being displayed
      */
     private var currentTheme = 0
+
+    /**
+     * Thread handler
+     */
+    private val handler = Handler()
 
     /**
      * View model
@@ -43,6 +51,13 @@ class HomeActivity : FragmentActivity() {
      * Last time picker dialog that was opened and is still active
      */
     var timePicker: TimePickerDialog? = null
+
+    /**
+     * Update state runnable
+     */
+    private val updateRunnable = Runnable {
+        updateState()
+    }
 
     /* ----------------------------------------------
      * FragmentActivity OVERRIDES
@@ -75,8 +90,17 @@ class HomeActivity : FragmentActivity() {
         })
     }
 
+    override fun onPause() {
+        super.onPause()
+
+        handler.removeCallbacks(updateRunnable)
+    }
+
     override fun onResume() {
         super.onResume()
+
+        // Clean up any previously active dialog reference
+        timePicker = null
 
         // Check that the theme has not changed while away
         if (currentTheme != getThemeResource()) {
@@ -105,20 +129,73 @@ class HomeActivity : FragmentActivity() {
      * Notified on click trigger when the run button is tapped
      */
     private fun runClick() {
-        // TODO
+        if (modelInfo.isActive())
+            model.resetAlarm()
+        else
+            model.setAlarm()
     }
 
     /**
      * Notified on click trigger when the time is tapped
      */
     private fun timeClick() {
-        // TODO
+        if (!modelInfo.isActive()) {
+            val timeListener = TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                model.setTime(hour = hourOfDay, minute = minute)
+            }
+
+            timePicker = TimePickerDialog(this, timeListener,
+                modelInfo.timeHour, modelInfo.timeMinute, DateFormat.is24HourFormat(applicationContext))
+            timePicker!!.show()
+        }
+    }
+
+    /**
+     * Assembles the string status for the time that is remaining before the alarm fires
+     * @return time remaining text
+     */
+    private fun timeTextRemaining(): String {
+        // Check if it should display in hours
+        val hoursToStop = modelInfo.getHoursTillAlarm()
+        if (hoursToStop > 1) {
+            return resources.getQuantityString(
+                R.plurals.alarm_notice_hours, hoursToStop.toInt(), hoursToStop
+            )
+        }
+
+        // Otherwise, it should display in minutes
+        val minutesToStop = modelInfo.getMinutesTillAlarm()
+        return resources.getQuantityString(
+            R.plurals.alarm_notice_minutes, minutesToStop.toInt(), minutesToStop
+        )
+    }
+
+    /**
+     * Updates the active state
+     */
+    private fun updateState() {
+        handler.removeCallbacks(updateRunnable)
+
+        if (modelInfo.isActive()) {
+            run_button.setImageResource(R.drawable.ic_stop)
+            time_text.setTextColor(getColor(R.color.textActive))
+            time_period.setTextColor(getColor(R.color.textActive))
+            time_remaining.text = timeTextRemaining()
+
+            handler.postDelayed(updateRunnable, UPDATE_TIME_MS)
+        } else {
+            run_button.setImageResource(R.drawable.ic_play)
+            time_text.setTextColor(getColor(R.color.textEdit))
+            time_period.setTextColor(getColor(R.color.textEdit))
+            time_remaining.text = ""
+        }
     }
 
     /**
      * Updates all views
      */
     private fun updateView() {
+        // Time setpoint
         val alarmTime = modelInfo.getTime()
         if (DateFormat.is24HourFormat(applicationContext)) {
             time_text.text = DateFormat.getTimeFormat(applicationContext).format(alarmTime.time)
@@ -128,5 +205,8 @@ class HomeActivity : FragmentActivity() {
             time_period.text = DateFormat.format("a", alarmTime).toString()
                 .replace(".", "")
         }
+
+        // Active state and time remaining
+        updateState()
     }
 }
